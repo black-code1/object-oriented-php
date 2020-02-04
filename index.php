8ea1b8ab6fca2fd209581b7ef1e46be8004b1e0e
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-04 13:02:04 +0100

    access modifiers

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 class User
 {
 
-  // properties methods
   public $username;
-  public $email;
+  private $email;
 
   public function __construct($username, $email)
   {
@@ -22,16 +21,12 @@ class User
 $userOne = new User('mario', 'mario@thenetninja.co.uk');
 $userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
-echo $userOne->username . '<br />';
-echo $userOne->email . '<br />';
-echo $userOne->addFriend() . '<br />';
+// $userOne->email = 3;
 
+// echo $userOne->email . '<br />';
+// echo $userTwo->email . '<br />';
 
-echo $userTwo->username . '<br />';
-echo $userTwo->email . '<br />';
-echo $userTwo->addFriend() . '<br />';
-// print_r(get_class_vars('User'));
-// print_r(get_class_methods('User'));
+echo $userOne->addFriend();
 
 ?>
 
