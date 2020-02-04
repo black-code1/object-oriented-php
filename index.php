5d2df679b5ec4cbde5699aefb5f66decbbda1d55
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-04 13:58:19 +0100

    working with static properties

--- index.php
+++ index.php
@@ -1,85 +1,30 @@
 <?php
 
-class User
+class Weather
 {
+  public static $tempConditions = ['cold', 'mild', 'warm'];
 
-  public $username;
-  protected $email;
-  public $role = 'member';
-
-  public function __construct($username, $email)
+  public static function celsiusToFarenheit($c)
   {
-    $this->username = $username;
-    $this->email = $email;
+    return $c * 9 / 5 + 32;
   }
 
-  public function __destruct()
+  public function determinTempCondition($f)
   {
-    echo "the user $this->username was removed" . '<br />';
-  }
-
-  public function __clone()
-  {
-    $this->username = $this->username . '(cloned)<br>';
-  }
-
-  public function addFriend()
-  {
-    return "$this->email added a new friend";
-  }
-
-  public function message()
-  {
-    return "$this->email send a new message";
-  }
-
-  // getters
-  public function getEmail()
-  {
-    return $this->email;
-  }
-
-  // setters
-  public function setEmail($email)
-  {
-    if (strpos($email, '@') > -1) {
-
-      $this->email = $email;
+    if ($f < 40) {
+      return self::$tempConditions[0];
+    } else if ($f < 70) {
+      return self::$tempConditions[1];
+    } else {
+      return self::$tempConditions[2];
     }
   }
 }
 
-class AdminUser extends User
-{
-  public $level;
-  public $role = 'admin';
-  // public $email;
 
-  public function __construct($username, $email, $level)
-  {
-    $this->level = $level;
-    parent::__construct($username, $email);
-    // $this->email = $email;
-  }
-
-  public function message()
-  {
-    return "$this->email, an admin, send a new message";
-  }
-}
-
-$userOne = new User('mario', 'mario@thenetninja.co.uk');
-$userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
-$userThree = new AdminUser('yoshi', 'yoshi@thenetninja.co.uk', 5);
-
-// unset($userOne);
-$userFour = clone $userOne;
-
-echo $userFour->username;
-
-
-
-
+// print_r(Weather::$tempConditions);
+// echo Weather::celsiusToFarenheit(20);
+echo Weather::determinTempCondition(50);
 
 ?>
 
