b42c8b46c55fd3263fe37a68776784b45f6a0ff0
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-04 12:32:11 +0100

    Starter Template to learn Object Oriented PHP

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+class User
+{
+
+  // properties methods
+
+}
+
+$userOne = new User();
+$userTwo = new User();
+
+echo 'the class is ' . get_class($userTwo);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Object Oriented PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
